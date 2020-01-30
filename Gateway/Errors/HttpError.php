2e685684fabbfb4675caa248942eb56de5d9dac3
Angel Barrientos <angel.barrientos@pagofacil.net>
2020-01-30 16:56:38 -0600

    Se modifica la excepción para un mejor acoplamiento con el sistema

--- Gateway/Errors/HttpError.php
+++ Gateway/Errors/HttpError.php
@@ -3,5 +3,16 @@
 
 class HttpError extends Error
 {
+    private $note;
 
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->note = $message;
+    }
+
+    public function getNote()
+    {
+        return $this->note;
+    }
 }
